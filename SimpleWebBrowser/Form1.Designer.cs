f27dac5537592490846829a1ea786292e677426c
Milos <milosvuksanovic53@gmail.com>
2021-04-09 19:57:46 +1000

    ...

---
+++ SimpleWebBrowser/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace SimpleWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookmarksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewBookmarkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hashSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeHashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchBar = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBoxHistory = new System.Windows.Forms.ListBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.addHashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.bookmarksToolStripMenuItem,
+            this.hashSettingsToolStripMenuItem,
+            this.clearListToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1368, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.newPageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // newPageToolStripMenuItem
+            // 
+            this.newPageToolStripMenuItem.Name = "newPageToolStripMenuItem";
+            this.newPageToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.newPageToolStripMenuItem.Text = "New Page";
+            this.newPageToolStripMenuItem.Click += new System.EventHandler(this.newPageToolStripMenuItem_Click);
+            // 
+            // bookmarksToolStripMenuItem
+            // 
+            this.bookmarksToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewBookmarkToolStripMenuItem});
+            this.bookmarksToolStripMenuItem.Name = "bookmarksToolStripMenuItem";
+            this.bookmarksToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.bookmarksToolStripMenuItem.Text = "Bookmarks";
+            // 
+            // addNewBookmarkToolStripMenuItem
+            // 
+            this.addNewBookmarkToolStripMenuItem.Name = "addNewBookmarkToolStripMenuItem";
+            this.addNewBookmarkToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNewBookmarkToolStripMenuItem.Text = "Add New Bookmark";
+            this.addNewBookmarkToolStripMenuItem.Click += new System.EventHandler(this.addNewBookmarkToolStripMenuItem_Click);
+            // 
+            // hashSettingsToolStripMenuItem
+            // 
+            this.hashSettingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeHashToolStripMenuItem,
+            this.addHashToolStripMenuItem});
+            this.hashSettingsToolStripMenuItem.Name = "hashSettingsToolStripMenuItem";
+            this.hashSettingsToolStripMenuItem.Size = new System.Drawing.Size(91, 20);
+            this.hashSettingsToolStripMenuItem.Text = "Hash Settings";
+            // 
+            // removeHashToolStripMenuItem
+            // 
+            this.removeHashToolStripMenuItem.Name = "removeHashToolStripMenuItem";
+            this.removeHashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.removeHashToolStripMenuItem.Text = "Remove Hash";
+            this.removeHashToolStripMenuItem.Click += new System.EventHandler(this.removeHashToolStripMenuItem_Click);
+            // 
+            // clearListToolStripMenuItem
+            // 
+            this.clearListToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.clearListToolStripMenuItem.Name = "clearListToolStripMenuItem";
+            this.clearListToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.clearListToolStripMenuItem.Text = "Clear List";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // searchBar
+            // 
+            this.searchBar.Location = new System.Drawing.Point(12, 27);
+            this.searchBar.Name = "searchBar";
+            this.searchBar.Size = new System.Drawing.Size(1344, 20);
+            this.searchBar.TabIndex = 1;
+            this.searchBar.Text = "http://";
+            this.searchBar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1344, 397);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.webBrowser1_ProgressChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 613);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1368, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(46, 17);
+            this.toolStripStatusLabel1.Text = "waiting";
+            // 
+            // listBoxHistory
+            // 
+            this.listBoxHistory.FormattingEnabled = true;
+            this.listBoxHistory.Location = new System.Drawing.Point(12, 456);
+            this.listBoxHistory.Name = "listBoxHistory";
+            this.listBoxHistory.Size = new System.Drawing.Size(1344, 134);
+            this.listBoxHistory.TabIndex = 7;
+            // 
+            // addHashToolStripMenuItem
+            // 
+            this.addHashToolStripMenuItem.Name = "addHashToolStripMenuItem";
+            this.addHashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addHashToolStripMenuItem.Text = "Add Hash";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 635);
+            this.Controls.Add(this.listBoxHistory);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.searchBar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Milo\'s Personal Web Browser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox searchBar;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem newPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookmarksToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewBookmarkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hashSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeHashToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.ListBox listBoxHistory;
+        private System.Windows.Forms.ToolStripMenuItem clearListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addHashToolStripMenuItem;
+    }
+}
+
---
+++ SimpleWebBrowser/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Diagnostics; //we need this to find out performance of the browser.
+using System.Security.Cryptography; //Security purposes/
+namespace SimpleWebBrowser
+{
+
+    public partial class Form1 : Form
+    {
+        public SpeechSynthesizer synth; //this is my assistant, he speaks when i ask him to. :Dff\d
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(searchBar.Text);
+        }
+
+
+        private void NavigateToPage()
+        {
+
+            toolStripStatusLabel1.Text = "Navigation Has Started.";
+          //  navigateButton.Enabled = false;
+            searchBar.Enabled = false;
+            webBrowser1.Navigate(searchBar.Text);
+
+
+
+            if (string.IsNullOrEmpty(searchBar.Text) == true) //is it empty? 
+            {
+                MessageBox.Show("Field cannot be empty."); // if it is empty, return this message.
+            } 
+            else if (searchBar.Text.Length != ' ')
+            {
+                //say Not hashed or HASHED.
+                string isHashed = searchBar.Text.GetHashCode().ToString();
+                isHashed = "(Hashed)";
+
+                webBrowser1.Navigate(searchBar.Text); //search.
+                listBoxHistory.Items.Add(searchBar.Text.GetHashCode() + " " + isHashed + " " + DateTime.Now);
+                Debug.WriteLine("Recently Added: " + searchBar.Text + " -- " + "Hashed Code: " + searchBar.Text.GetHashCode()); // should print to console - whatever was added by user.
+            }
+
+         
+
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter) //by pressing enter we will navigate to the page desired/Entered by the user.
+            {
+                NavigateToPage(); // this is where it will lead to.
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            //navigateButton.Enabled = true;
+            searchBar.Enabled = true;
+            toolStripStatusLabel1.Text = "Completed";
+
+        }
+
+        //loading bar
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.CurrentProgress > 0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+
+            }
+
+        }
+
+        private void newPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //show new page.
+            var form1 = new Form1();
+            form1.Show();
+        }
+
+        private void openNewTabWithKeys(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Tab && e.KeyChar == (char)ConsoleKey.N)
+            {
+                MessageBox.Show("Tab an N pressed");
+            }
+           
+        }
+        private void openListBoxHistory_Click(object sender, EventArgs e, KeyEventArgs k)
+        {
+
+            if (searchBar.Text.Length != ' ')
+            {
+                listBoxHistory.Items.Add(searchBar.Text);
+
+            }
+
+        }
+
+        //private void clearListButton_Click(object sender, EventArgs e)
+        //{
+          //  if (listBoxHistory.Items.Count > 0) //clear search history.
+          //  {
+            //    listBoxHistory.Items.Clear(); //clear listed items in History Sect.
+             //   MessageBox.Show("Items Cleared");
+           // }
+            //else if (listBoxHistory.Items.Count <= 0) //if nothing inside history, you cant clear.
+            //{
+              //  MessageBox.Show("There must be something to clear");
+            //}
+
+        //}
+
+        private void addNewBookmarkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (addNewBookmarkToolStripMenuItem.Pressed)
+            {
+
+                //MessageBox.Show("Added to Bookmarks");
+                //listBoxHistory.Items.Add(searchBar.Text + " " +"(Bookmarked item)"); //right it works - but now how we want..
+                MessageBox.Show(searchBar.Text + " was added to bookmarks!");
+                addNewBookmarkToolStripMenuItem.DropDownItems.Add(searchBar.Text);
+
+            }
+        }
+
+
+        private void removeHashToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (removeHashToolStripMenuItem.Pressed)
+            {
+                if (searchBar.Text.Length > 0) {
+                    listBoxHistory.Refresh(); // refresh collection
+                    listBoxHistory.Items.Clear(); //clear collection
+                    listBoxHistory.Items.Add(searchBar.Text + " " + DateTime.Now); //add search item not hashed.
+
+                }
+                else
+                {
+                    MessageBox.Show("there needs to be something to remove.");
+                }
+
+            }
+            
+        }
+
+
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(searchBar.Text) == false) //if it it has something in it
+            {
+
+
+
+                //testToolStripMenuItem.DropDownItems.Add(searchBar.Text);
+
+
+            }
+
+        }
+
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Clear items from list using the drop-down menu button
+            if (listBoxHistory.Items.Count > 0)
+            {
+                listBoxHistory.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("There needs to be something to Delete");
+                MessageBox.Show("Add a link to remove");
+            }
+        }
+
+    }
+
+       
+    }
+
+
+
